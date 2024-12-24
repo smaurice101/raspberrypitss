78f3a6d37a7b26112d5a38658958ba5d94850d68
smaurice101 <smaurice101@email>
2024-12-24 23:15:55 +0000

    Committing raspberrypitss

--- tml-airflow/dags/message.py
+++ tml-airflow/dags/message.py
@@ -3,6 +3,7 @@ from airflow.decorators import dag, task
 
 # Instantiate your DAG
 @dag(dag_id="Aloha_Please_Wait_For_Dags_To_Populate",default_args={}, tags=[""],schedule=None,catchup=False)
-def startproducingtotopic():
+def message():
    def empty():
      pass
+dag = message()
