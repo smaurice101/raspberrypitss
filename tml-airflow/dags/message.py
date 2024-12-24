c0f68ff85b95a5dd4ee3a8a56a1e74b915d918e5
smaurice101 <smaurice101@email>
2024-12-24 23:06:55 +0000

    Committing raspberrypitss

--- tml-airflow/dags/message.py
+++ tml-airflow/dags/message.py
@@ -1,5 +1,6 @@
 from airflow import DAG
-
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 
 # Instantiate your DAG
 @dag(dag_id="Aloha_Please_Wait_For_Dags_To_Populate",schedule=None,catchup=False)
