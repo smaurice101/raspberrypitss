b964e1cdae69e65dcad1b83d8bdea092940918bd
smaurice101 <smaurice101@email>
2025-01-14 01:06:24 +0000

    Committing raspberrypitss

--- tml-airflow/dags/tsslogging.py
+++ tml-airflow/dags/tsslogging.py
@@ -566,7 +566,7 @@ def optimizecontainer(cname,sname,sd):
         ret=subprocess.check_output(greps, shell=True)        
         ret=ret.decode("utf-8")
         ret=ret.strip()
-        pritn("cname=",cname)
+        print("cname=",cname)
         print("ret=",ret)
         
         if cname not in ret:
