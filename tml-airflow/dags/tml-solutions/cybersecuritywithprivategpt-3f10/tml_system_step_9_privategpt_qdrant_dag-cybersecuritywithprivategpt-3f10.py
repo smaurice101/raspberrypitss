1caf8c0c07a35f305936e62b2289fe7a9d8ee621
smaurice101 <smaurice101@email>
2025-01-20 13:22:10 +0000

    Committing raspberrypitss

--- tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_9_privategpt_qdrant_dag-cybersecuritywithprivategpt-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_9_privategpt_qdrant_dag-cybersecuritywithprivategpt-3f10.py
@@ -49,10 +49,10 @@ anomaly probabilities are less than 0.60, it is likely the risk of a cyber attac
  'vectordbcollectionname' : 'tml', # change as needed
  'concurrency' : '1', # change as needed Leave at 1
  'CUDA_VISIBLE_DEVICES' : '0', # change as needed
- 'docfolder': '',  # You can specify the sub-folder that contains TEXT or PDF files..this is a subfolder in the MAIN folder mapped to /rawdata
+ 'docfolder': 'mylogs,mylogs2',  # You can specify the sub-folder that contains TEXT or PDF files..this is a subfolder in the MAIN folder mapped to /rawdata
                    # if this field in NON-EMPTY, privateGPT will query these documents as the CONTEXT to answer your prompt
                    # separate multiple folders with a comma
- 'docfolderingestinterval': '', # how often you want TML to RE-LOAD the files in docfolder - enter the number of SECONDS
+ 'docfolderingestinterval': '60', # how often you want TML to RE-LOAD the files in docfolder - enter the number of SECONDS
  'useidentifierinprompt': '1', # If 1, this uses the identifier in the TML json output and appends it to prompt, If 0, it uses the prompt only    
 }
 
