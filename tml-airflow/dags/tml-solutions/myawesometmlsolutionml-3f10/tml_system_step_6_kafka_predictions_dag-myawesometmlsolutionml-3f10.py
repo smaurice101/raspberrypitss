488e1725a168550c39cd96c3ee63d03753161847
smaurice101 <smaurice101@email>
2025-01-22 21:02:26 +0000

    Committing raspberrypitss

--- tml-airflow/dags/tml-solutions/myawesometmlsolutionml-3f10/tml_system_step_6_kafka_predictions_dag-myawesometmlsolutionml-3f10.py
+++ tml-airflow/dags/tml-solutions/myawesometmlsolutionml-3f10/tml_system_step_6_kafka_predictions_dag-myawesometmlsolutionml-3f10.py
@@ -27,7 +27,7 @@ default_args = {
   'mylocation' : 'Your location', # <<< *** Change as needed      
   'brokerhost' : '', # <<< *** Leave as is 
   'brokerport' : '-999', # <<< *** Leave as is
-  'streamstojoin' : 'Power_preprocessed_AnomProb,Current_preprocessed_AnomProb', # << ** These are the streams in the preprocess_data_topic for these independent variables
+  'streamstojoin' : 'Power_preprocessed_Avg', # << ** These are the streams in the preprocess_data_topic for these independent variables
   'inputdata' : '', # << ** You can specify independent variables manually - rather than consuming from the preprocess_data_topic stream
   'consumefrom' : 'ml-data', # << This is ml_data_topic in STEP 5 that contains the estimated parameters
   'mainalgokey' : '', # leave blank
