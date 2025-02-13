426ec3bd1bdf3e86dfe2cc09f3f91847b099ce85
smaurice101 <smaurice101@email>
2025-02-13 16:29:02 +0000

    Committing raspberrypitss

--- tml-airflow/dags/tml-solutions/elastalink-3f10/tml_system_step_5_kafka_machine_learning_dag-elastalink-3f10.py
+++ tml-airflow/dags/tml-solutions/elastalink-3f10/tml_system_step_5_kafka_machine_learning_dag-elastalink-3f10.py
@@ -34,14 +34,14 @@ default_args = {
   'networktimeout' : '600', # <<< *** Change as needed      
   'modelsearchtuner' : '90', # <<< *This parameter will attempt to fine tune the model search space - A number close to 100 means you will have fewer models but their predictive quality will be higher.      
   'dependentvariable' : 'failure', # <<< *** Change as needed, 
-  'independentvariables': 'BandwidthUtilization_preprocessed_Avg', # <<< *** Change as needed, 
+  'independentvariables': 'CPULoad_preprocessed_Avg', # <<< *** Change as needed, 
   'rollbackoffsets' : '1200', # <<< *** Change as needed, 
   'consumeridtrainingdata2': '', # leave blank
   'partition_training' : '',  # leave blank
   'consumefrom' : '',  # leave blank
   'topicid' : '-1',  # leave as is
   'fullpathtotrainingdata' : '/Viper-ml/viperlogs/elastalink',  #  # <<< *** Change as needed - add name for foldername that stores the training datasets
-  'processlogic' : 'classification_name=failure_prob:BandwidthUtilization_preprocessed_Avg=900,n',  # <<< *** Change as needed, i.e. classification_name=failure_prob:Voltage_preprocessed_AnomProb=55,n:Current_preprocessed_AnomProb=55,n
+  'processlogic' : 'classification_name=failure_prob:CPULoad_preprocessed_Avg=50,n',  # <<< *** Change as needed, i.e. classification_name=failure_prob:Voltage_preprocessed_AnomProb=55,n:Current_preprocessed_AnomProb=55,n
   'array' : '0',  # leave as is
   'transformtype' : '', # Sets the model to: log-lin,lin-log,log-log
   'sendcoefto' : '',  # you can send coefficients to another topic for further processing -- MUST BE SET IN STEP 2
