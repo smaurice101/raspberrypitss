bb64b944fb1e406ac57c9c64d7bae4348575e7f6
smaurice101 <smaurice101@email>
2025-02-13 20:44:11 +0000

    Committing raspberrypitss

--- tml-airflow/dags/tml-solutions/elastalink-3f10/tml_system_step_7_kafka_visualization_dag-elastalink-3f10.py
+++ tml-airflow/dags/tml-solutions/elastalink-3f10/tml_system_step_7_kafka_visualization_dag-elastalink-3f10.py
@@ -14,8 +14,8 @@ import random
 sys.dont_write_bytecode = True
 ######################################## USER CHOOSEN PARAMETERS ########################################
 default_args = {
-  'topic' : 'iot-preprocess,iot-ml-prediction-results-output',    # <<< *** Separate multiple topics by a comma - Viperviz will stream data from these topics to your browser
-  'dashboardhtml': 'iot-failure-machinelearning-elastalink.html',#'iot-failure-elastalink.html', # <<< *** name of your dashboard html file  try: iot-failure-seneca.html
+  'topic' : 'iot-preprocess',    # <<< *** Separate multiple topics by a comma - Viperviz will stream data from these topics to your browser
+  'dashboardhtml': 'iot-failure-elastalink.html',#'iot-failure-elastalink.html', # <<< *** name of your dashboard html file  try: iot-failure-seneca.html
   'secure': '1',   # <<< *** 1=connection is encrypted, 0=no encryption
   'offset' : '-1',    # <<< *** -1 indicates to read from the last offset always
   'append' : '0',   # << ** Do not append new data in the browser
