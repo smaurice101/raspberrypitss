679af052e874ef55fe07268c719dd431b235f4b0
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2025-02-17 14:51:59 -0500

    Update tml_read_LOCALFILE_step_3_kafka_producetotopic_dag-elastalink-3f10.py

--- tml-airflow/dags/tml-solutions/elastalink-3f10/tml_read_LOCALFILE_step_3_kafka_producetotopic_dag-elastalink-3f10.py
+++ tml-airflow/dags/tml-solutions/elastalink-3f10/tml_read_LOCALFILE_step_3_kafka_producetotopic_dag-elastalink-3f10.py
@@ -11,6 +11,10 @@ import subprocess
 import json 
 import time
 import random 
+import threading
+from contextlib import contextmanager
+from contextlib import ExitStack
+import re
 
 sys.dont_write_bytecode = True
 ######################################## USER CHOOSEN PARAMETERS ########################################
@@ -20,11 +24,15 @@ default_args = {
   'microserviceid' : '', # <<< *** leave blank
   'producerid' : 'iotsolution',   # <<< *** Change as needed   
   'topics' : 'iot-raw-data', # *************** This is one of the topic you created in SYSTEM STEP 2
-  'identifier' : 'TML solution for Elastalink',   # <<< *** Change as needed   
-  'inputfile' : '/rawdata/elastalink/networkLogs2.txt',  # <<< ***** replace ?  to input file name to read. NOTE this data file should be JSON messages per line and stored in the HOST folder mapped to /rawdata folder 
+  'identifier' : 'TML solution',   # <<< *** Change as needed   
+  'inputfile' : '/rawdatademo/IoTData.txt',  # <<< ***** replace ?  to input file name to read. NOTE this data file should be JSON messages per line and stored in the HOST folder mapped to /rawdata folder 
   'delay' : '7000', # << ******* 7000 millisecond maximum delay for VIPER to wait for Kafka to return confirmation message is received and written to topic
   'topicid' : '-999', # <<< ********* do not modify  
   'sleep' : 0.15, # << Control how fast data streams - if 0 - the data will stream as fast as possible - BUT this may cause connecion reset by peer 
+  'docfolder' : '', # You can read TEXT files or any file in these folders that are inside the volume mapped to /rawdata
+  'doctopic' : '',  # This is the topic that will contain the docfolder file data
+  'chunks' : 0, # if 0 the files in docfolder are read line by line, otherwise they are read by chunks i.e. 512
+  'docingestinterval' : 0, # specify the frequency in seconds to read files in docfolder - if 0 the files are read ONCE
 }
 
 ######################################## DO NOT MODIFY BELOW #############################################
