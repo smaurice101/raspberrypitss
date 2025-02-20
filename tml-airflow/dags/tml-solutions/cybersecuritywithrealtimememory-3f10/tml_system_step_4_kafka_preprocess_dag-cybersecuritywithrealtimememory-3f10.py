b57009fa44577f51677b9e84c302f2517e1f5d2b
smaurice101 <smaurice101@email>
2025-02-20 01:28:02 +0000

    Committing raspberrypitss

--- tml-airflow/dags/tml-solutions/cybersecuritywithrealtimememory-3f10/tml_system_step_4_kafka_preprocess_dag-cybersecuritywithrealtimememory-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecuritywithrealtimememory-3f10/tml_system_step_4_kafka_preprocess_dag-cybersecuritywithrealtimememory-3f10.py
@@ -36,7 +36,7 @@ default_args = {
   'tmlfilepath' : '', # leave blank
   'usemysql' : '1', # do not modify
   'streamstojoin' : '', # leave blank
-  'identifier' : 'IoT device performance and failures', # <<< ** Change as needed
+  'identifier' : 'RTMS Cybersecurity Prevention', # <<< ** Change as needed
   'preprocesstypes' : 'anomprob,trend,avg', # <<< **** MAIN PREPROCESS TYPES CHNAGE AS NEEDED refer to https://tml-readthedocs.readthedocs.io/en/latest/
   'pathtotmlattrs' : 'oem=n/a,lat=n/a,long=n/a,location=n/a,identifier=n/a', # Change as needed     
   'jsoncriteria' : 'uid=hostName,filter:allrecords~\
