9ba8255b9431dcf53aa55cd84e7dc16e1f45f01a
smaurice101 <smaurice101@email>
2025-02-23 16:05:55 +0000

    Committing raspberrypitss

--- tml-airflow/dags/tml-solutions/cybersecurityrtms-3f10/tml_system_step_4c_kafka_preprocess_dag-cybersecurityrtms-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecurityrtms-3f10/tml_system_step_4c_kafka_preprocess_dag-cybersecurityrtms-3f10.py
@@ -234,6 +234,7 @@ if __name__ == '__main__':
             processtransactiondata()
             time.sleep(1)
           except Exception as e:     
+           print("errror=",e)
            tsslogging.locallogs("ERROR", "STEP 4c: Preprocessing3 DAG in {} {}".format(os.path.basename(__file__),e))
            tsslogging.tsslogit("Preprocessing3 DAG in {} {}".format(os.path.basename(__file__),e), "ERROR" )                     
            tsslogging.git_push("/{}".format(repo),"Entry from {}".format(os.path.basename(__file__)),"origin")    
