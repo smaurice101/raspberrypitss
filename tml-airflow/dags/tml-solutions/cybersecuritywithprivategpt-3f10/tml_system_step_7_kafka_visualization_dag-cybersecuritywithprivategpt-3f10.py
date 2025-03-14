9bc9bcd51dc4b93210f5a4a6fd15eb7b5b08347b
smaurice101 <smaurice101@email>
2025-03-14 21:26:24 +0000

    Committing raspberrypitss

--- tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_7_kafka_visualization_dag-cybersecuritywithprivategpt-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecuritywithprivategpt-3f10/tml_system_step_7_kafka_visualization_dag-cybersecuritywithprivategpt-3f10.py
@@ -19,7 +19,7 @@ default_args = {
   'dashboardhtml-ml': '', # <<< *** This one is IF you include ML dag
   'topic-ml' : '',    # <<< *** Separate multiple topics by a comma
   'dashboardhtml-ai': 'tml-cisco-network-privategpt-monitor.html', # <<< *** This one is you include AI dag
-  'topic-ai' : '',    # <<< *** Separate multiple topics by a comma    
+  'topic-ai' : 'cisco-network-preprocess,cisco-network-privategpt',    # <<< *** Separate multiple topics by a comma    
   'dashboardhtml-ml-ai': '', # <<< *** This one is you include ML-AI dag
   'topic-ml-ai' : '',    # <<< *** Separate multiple topics by a comma            
   'secure': '1',   # <<< *** 1=connection is encrypted, 0=no encryption
