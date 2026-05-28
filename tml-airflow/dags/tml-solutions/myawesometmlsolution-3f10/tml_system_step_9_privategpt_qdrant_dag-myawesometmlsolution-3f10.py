2338b8815c9cf8ceabe43b2964cfaf21b3264fb1
smaurice101 <smaurice101@email.local>
2026-05-28 01:37:29 +0000

    Committing raspberrypitss changes via automation script

--- tml-airflow/dags/tml-solutions/myawesometmlsolution-3f10/tml_system_step_9_privategpt_qdrant_dag-myawesometmlsolution-3f10.py
+++ tml-airflow/dags/tml-solutions/myawesometmlsolution-3f10/tml_system_step_9_privategpt_qdrant_dag-myawesometmlsolution-3f10.py
@@ -41,7 +41,7 @@ default_args = {
     'prompt': '[INST] Are there any errors in the  logs? Give s detailed response including IP addresses and host machines.[/INST]', # Enter your prompt here
     'context': """This is network data from inbound and outbound packets. The data are
 anomaly probabilities for cyber threats from analysis of inbound and outbound packets. If inbound or outbound
-anomaly probabilities are less than 0.60, it is likely the risk of a cyber attack is also low. If its above 0.60, then risk is mid to high."""  # what is this data about? Provide context to PrivateGPT
+anomaly probabilities are less than 0.60, it is likely the risk of a cyber attack is also low. If its above 0.60, then risk is mid to high.""",  # what is this data about? Provide context to PrivateGPT
     'jsonkeytogather': 'hyperprediction', # enter key you want to gather data from to analyse with PrivateGpt i.e. Identifier or hyperprediction
     'keyattribute': 'inboundpackets,outboundpackets', # change as needed
     'keyprocesstype': 'anomprob', # change as needed
