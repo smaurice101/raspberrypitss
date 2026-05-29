f31ad9023f151d9130f6ca80ea9e54d990cb9c20
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2026-05-28 21:40:25 -0400

    Update tml_system_step_8_deploy_solution_to_docker_dag-cybersecurityrtms-3f10.py

--- tml-airflow/dags/tml-solutions/cybersecurityrtms-3f10/tml_system_step_8_deploy_solution_to_docker_dag-cybersecurityrtms-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecurityrtms-3f10/tml_system_step_8_deploy_solution_to_docker_dag-cybersecurityrtms-3f10.py
@@ -77,10 +77,15 @@ def dockerit(**context):
            tsslogging.locallogs("INFO", "STEP 8: Docker Container created and optimized.  Will push it now.  Here is the commit command: {} - message={}".format(cbuf,v))         
            
          #v=subprocess.call("docker push {}".format(cname), shell=True) 
-         proc=subprocess.Popen("docker push {}".format(cname), shell=True)
-         time.sleep(3)   
-         proc.terminate()
-         proc.wait()
+         proc = subprocess.Popen(
+            ["docker", "push", cname],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+         )             
+         #proc=subprocess.Popen("docker push {}".format(cname), shell=True)
+         #time.sleep(3)   
+         #proc.terminate()
+         #proc.wait()
 
        elif len(cid) <= 1:
               tsslogging.locallogs("ERROR", "STEP 8: There seems to be an issue with docker commit. Here is the command: docker commit {} {}".format(cid,cname)) 
