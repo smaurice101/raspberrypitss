5e10b14c34fda60f9f71abfe14a14fea13cbecff
smaurice101 <smaurice101@email.local>
2026-06-09 14:51:51 +0000

    Committing raspberrypitss changes via automation script

--- tml-airflow/dags/tml-solutions/cybersecurity-rtms-main-3f10/tml_read_LOCALFILE_step_3_kafka_producetotopic_dag-cybersecurity-rtms-main-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecurity-rtms-main-3f10/tml_read_LOCALFILE_step_3_kafka_producetotopic_dag-cybersecurity-rtms-main-3f10.py
@@ -19,7 +19,7 @@ import re
 sys.dont_write_bytecode = True
 ######################################## USER CHOOSEN PARAMETERS ########################################
 default_args = {
-    'owner': 'Sebastian aaa Maurice', # <<< *** Change as needed
+    'owner': 'Sebastian bbb Maurice', # <<< *** Change as needed
     'enabletls': 1, # <<< *** 1=connection is encrypted, 0=no encryption
     'microserviceid': '', # <<< *** leave blank
     'producerid': 'iotsolution22', # <<< *** Change as needed
