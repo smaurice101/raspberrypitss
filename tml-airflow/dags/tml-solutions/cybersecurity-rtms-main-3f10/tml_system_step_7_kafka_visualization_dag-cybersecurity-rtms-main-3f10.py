a0ed630865bb3eda787ec7235bec11f9bc0726e3
smaurice101 <smaurice101@email.local>
2026-06-10 13:24:02 +0000

    Committing raspberrypitss changes via automation script

--- tml-airflow/dags/tml-solutions/cybersecurity-rtms-main-3f10/tml_system_step_7_kafka_visualization_dag-cybersecurity-rtms-main-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecurity-rtms-main-3f10/tml_system_step_7_kafka_visualization_dag-cybersecurity-rtms-main-3f10.py
@@ -14,18 +14,18 @@ import random
 sys.dont_write_bytecode = True
 ######################################## USER CHOOSEN PARAMETERS ########################################
 default_args = {
-  'topic' : 'iot-preprocess,iot-preprocess2',    # <<< *** Separate multiple topics by a comma - Viperviz will stream data from these topics to your browser
-  'dashboardhtml': 'rtmscyber.html', # <<< *** name of your dashboard file: This one is ONLY for preprocessing
-  'dashboardhtml-ml': 'dashboard-ml.html', # <<< *** This one is IF you include ML dag
-  'topic-ml' : 'iot-preprocess,iot-preprocess2',    # <<< *** Separate multiple topics by a comma
-  'dashboardhtml-ai': 'dashboard-ai.html', # <<< *** This one is you include AI dag
-  'topic-ai' : 'iot-preprocess,iot-preprocess2',    # <<< *** Separate multiple topics by a comma  
-  'dashboardhtml-ml-ai': 'dashboard-ml-ai.html', # <<< *** This one is you include ML-AI dag
-  'topic-ml-ai' : 'iot-preprocess,iot-preprocess2',    # <<< *** Separate multiple topics by a comma    
-  'secure': '1',   # <<< *** 1=connection is encrypted, 0=no encryption
-  'offset' : '-1',    # <<< *** -1 indicates to read from the last offset always
-  'append' : '0',   # << ** Do not append new data in the browser
-  'rollbackoffset' : '400', # *************** Rollback the data stream by rollbackoffset.  For example, if 500, then Viperviz wll grab all of the data from the last offset - 500
+    'topic': 'iot-preprocess,iot-preprocess2', # <<< *** Separate multiple topics by a comma - Viperviz will stream data from these topics to your browser
+    'dashboardhtml': 'rtmscyber.html', # <<< *** name of your dashboard file: This one is ONLY for preprocessing
+    'dashboardhtml-ml': 'dashboard-ml.html', # <<< *** This one is IF you include ML dag
+    'topic-ml': 'iot-preprocess,iot-preprocess2', # <<< *** Separate multiple topics by a comma
+    'dashboardhtml-ai': 'dashboard-ai.html', # <<< *** This one is you include AI dag
+    'topic-ai': 'iot-preprocess,iot-preprocess2', # <<< *** Separate multiple topics by a comma
+    'dashboardhtml-ml-ai': 'dashboard-ml-ai.html', # <<< *** This one is you include ML-AI dag
+    'topic-ml-ai': 'iot-preprocess,iot-preprocess2', # <<< *** Separate multiple topics by a comma
+    'secure': 1, # <<< *** 1=connection is encrypted, 0=no encryption
+    'offset': -1, # <<< *** -1 indicates to read from the last offset always
+    'append': 0, # << ** Do not append new data in the browser
+    'rollbackoffset': 400, # *************** Rollback the data stream by rollbackoffset.  For example, if 500, then Viperviz wll grab all of the data from the last offset - 500
 }
 
 ######################################## DO NOT MODIFY BELOW #############################################
