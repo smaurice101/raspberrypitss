377ba70817a74f9bbd15a03ffc55439dbdea81f6
smaurice101 <smaurice101@email.local>
2026-06-12 00:56:26 +0000

    Committing raspberrypitss changes via automation script

--- tml-airflow/dags/tml-solutions/cybersecurity-rtms-main-3f10/tml_system_step_2_kafka_createtopic_dag-cybersecurity-rtms-main-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecurity-rtms-main-3f10/tml_system_step_2_kafka_createtopic_dag-cybersecurity-rtms-main-3f10.py
@@ -29,7 +29,7 @@ default_args = {
     'ml_data_topic': 'ml-data', # Separate multiple topics with comma <<< ********** You change topic names as needed
     'prediction_data_topic': 'prediction-data', # Separate multiple topics with comma <<< ********** You change topic names as needed
     'pgpt_data_topic': 'cisco-network-privategpt', # PrivateGPT will produce responses to this topic - change as  needed
-    'descriptions': 'Topics to store iot data',
+    'description': 'Topics to store iot data',
 }
 
 ######################################## DO NOT MODIFY BELOW #############################################
