f8b5c0e7ef1c83717714fcbd8cae43286f5efd73
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2026-06-19 17:29:07 -0400

    Update tml_system_step_4c_kafka_preprocess_dag-cybersecurity-rtms-entity-3f10.py

--- tml-airflow/dags/tml-solutions/cybersecurity-rtms-entity-3f10/tml_system_step_4c_kafka_preprocess_dag-cybersecurity-rtms-entity-3f10.py
+++ tml-airflow/dags/tml-solutions/cybersecurity-rtms-entity-3f10/tml_system_step_4c_kafka_preprocess_dag-cybersecurity-rtms-entity-3f10.py
@@ -22,7 +22,7 @@ default_args = {
   'enabletls': '1', # <<< *** 1=connection is encrypted, 0=no encryption
   'microserviceid' : '',  # <<< *** leave blank
   'producerid' : 'rtmssolution',   # <<< *** Change as needed   
-  'raw_data_topic' : 'iot-preprocess', # *************** INCLUDE ONLY ONE TOPIC - This is one of the topic you created in SYSTEM STEP 2
+  'raw_data_topic' : 'rtms-entity-preprocess', # *************** INCLUDE ONLY ONE TOPIC - This is one of the topic you created in SYSTEM STEP 2
   'preprocess_data_topic' : 'rtms-preprocess', # *************** INCLUDE ONLY ONE TOPIC - This is one of the topic you created in SYSTEM STEP 2
   'maxrows' : '200', # <<< ********** Number of offsets to rollback the data stream -i.e. rollback stream by 500 offsets
   'offset' : '-1', # <<< Rollback from the end of the data streams  
@@ -43,7 +43,7 @@ default_args = {
   'searchterms' : 'rgx:p([a-z]+)ch ~~~ |authentication failure,--entity-- password failure ~~~ |unknown--entity--', # main Search terms, if AND add @, if OR use | s first characters, default OR
                                                              # Must include --entity-- if correlating with entity - this will be replaced 
                                                              # dynamically with the entities found in raw_data_topic
-  'localsearchtermfolder': '|/rawdatademo/mysearchfile1demo,|/rawdatademo/mysearchfile2demo', # Specify a folder of files containing search terms - each term must be on a new line - use comma
+  'localsearchtermfolder': '|/rawdata/mysearchfile1,|/rawdata/mysearchfile2', # Specify a folder of files containing search terms - each term must be on a new line - use comma
                                # to apply each folder to the rtmstream topic
                                # Use @ =AND, |=OR to specify whether the terms in the file should be AND, OR
                                # For example, @mysearchfolder1,|mysearchfolder2, means all terms in mysearchfolder1 should be AND
