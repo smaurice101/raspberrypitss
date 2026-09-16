743c4ca58189ef319af4e64a1572c32273ed0358
smaurice101 <smaurice101@email.local>
2026-09-16 13:11:28 +0000

    Committing raspberrypitss changes via automation script

--- tml-airflow/dags/tml-solutions/rogue-agents-cncf-3f10/tml_system_step_7_kafka_visualization_dag-rogue-agents-cncf-3f10.py
+++ tml-airflow/dags/tml-solutions/rogue-agents-cncf-3f10/tml_system_step_7_kafka_visualization_dag-rogue-agents-cncf-3f10.py
@@ -25,7 +25,7 @@ default_args = {
     'secure': 1, # <<< *** 1=connection is encrypted, 0=no encryption
     'offset': -1, # <<< *** -1 indicates to read from the last offset always
     'append': 0, # << ** Do not append new data in the browser
-    'rollbackoffset': 50, # *************** Rollback the data stream by rollbackoffset.  For example, if 500, then Viperviz wll grab all of the data from the last offset - 500
+    'rollbackoffset': 51, # *************** Rollback the data stream by rollbackoffset.  For example, if 500, then Viperviz wll grab all of the data from the last offset - 500
 }
 
 ######################################## DO NOT MODIFY BELOW #############################################
